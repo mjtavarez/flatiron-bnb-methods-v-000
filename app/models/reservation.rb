f946823dd2c96dd71060f2c0521fc3a41d91ec18
Miguel Tavarez <tavarezster@gmail.com>
2017-06-01 21:27:07 +0000

    helper methods and validations for Reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,16 +3,39 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
   
-  validates :checkin, :checkout, presence: true
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+  validate :available?, :checkin_before_checkout
+
+  
+  before_validation :is_not_host
   
   def duration
     (checkout - checkin).to_i
   end
   
-  private
+  def total_price
+    (listing.price)*duration
+  end
   
-    def host?
-      guest_id == host_id
+  private
+    def is_not_host
+      Listing.find(self.listing_id).host_id != self.guest_id
+    end
+    
+    def available?
+      Reservation.where(listing_id: listing.id).where.not(id: id).each do |r|
+        booked_dates = r.checkin..r.checkout
+        if booked_dates.include?(checkin) || booked_dates.include?(checkout)
+          errors.add(:guest_id, "Sorry, this place isn't available during your requested dates.")
+        end
+      end
+    end
+    
+    def checkin_before_checkout
+      if checkout && checkin && checkout <= checkin
+        errors.add(:guest_id, "You must checkout after you checkin")
+      end
     end
   
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,12 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
   
-  validates :rating, :description, presence: true
+  validates :rating, :description, :reservation_id, presence: true
+  validate :checked_out
+  
+  private
+    def checked_out
+      Reservation.find(reservation.id).checkout < Date.today
+    end
 
 end
